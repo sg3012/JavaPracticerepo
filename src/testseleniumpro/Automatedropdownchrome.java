9599773186301a13a240074f77e35b2471c70056
sg3012 <sgnight30@gmail.com>
2019-10-09 18:18:20 +0530

    code for handling a drop in chrome browser commited

---
+++ src/testseleniumpro/Automatedropdownchrome.java
@@ -0,0 +1,25 @@
+package testseleniumpro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Automatedropdownchrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.setProperty("webdriver.chrome.driver","D:\\Automation\\ChromeDriver-77.0\\chromedriver.exe");
+    WebDriver launchchrome = new ChromeDriver();
+    launchchrome.get("https://www.facebook.com");
+   /* //1. Handling drop-down using "id" locator. 
+    Select obj1 = new Select(launchchrome.findElement(By.id("month")));
+    obj1.selectByVisibleText("Jan"); */ 
+    
+  //1. Handling drop-down using "name" locator. 
+    Select obj1 = new Select(launchchrome.findElement(By.name("birthday_month")));
+    obj1.selectByVisibleText("Feb");
+    
+	}
+
+}
