f348a2315d5649bf71043901ceb0d441fed2f64f
sg3012 <sgnight30@gmail.com>
2019-10-11 18:23:52 +0530

    code to automate signup form partially pushed-Chrome/FF

---
+++ src/testseleniumpro/TestsignupflowhappypathChrome.java
@@ -0,0 +1,26 @@
+package testseleniumpro;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestsignupflowhappypathChrome {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\Automation\\ChromeDriver-77.0\\chromedriver.exe");
+		 WebDriver launchchrome = new ChromeDriver();
+		 launchchrome.get("https://www.incommail.com/"); 
+		 Thread.sleep(2000);
+		 launchchrome.findElement(By.linkText("Sign Up")).click(); 
+		 launchchrome.findElement(By.id("emailReg")).sendKeys("automateoct1105@yopmail.com");
+		 Thread.sleep(3000);
+		 launchchrome.findElement(By.id("passwordReg")).sendKeys("test12345");
+		 Thread.sleep(3000);
+		 launchchrome.findElement(By.id("confirm_passwordReg")).sendKeys("test12345");
+		 Thread.sleep(2000);
+		 launchchrome.findElement(By.id("agree")).click();
+		 Thread.sleep(2000);
+		 launchchrome.findElement(By.cssSelector("#otpsignup > form > button")).click();
+	}
+
+}
---
+++ src/testseleniumpro/TestsignupflowhappypathFF.java
@@ -0,0 +1,27 @@
+package testseleniumpro;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class TestsignupflowhappypathFF {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","D:\\Automation\\geckodriver-0.25.0\\geckodriver.exe");
+		WebDriver launchfirefox = new FirefoxDriver();
+		launchfirefox.get("https://www.incommail.com/"); 
+		Thread.sleep(2000);
+		launchfirefox.findElement(By.linkText("Sign Up")).click();
+		Thread.sleep(1000);
+		launchfirefox.findElement(By.id("emailReg")).sendKeys("automateoct1106@yopmail.com");
+		Thread.sleep(3000);
+		launchfirefox.findElement(By.id("passwordReg")).sendKeys("test12345");
+		Thread.sleep(3000);
+		launchfirefox.findElement(By.id("confirm_passwordReg")).sendKeys("test12345");
+		Thread.sleep(2000);
+		launchfirefox.findElement(By.id("agree")).click();
+		Thread.sleep(2000);
+		launchfirefox.findElement(By.cssSelector("#otpsignup > form:nth-child(2) > button:nth-child(5)")).click();
+		
+	}
+
+}
