0275e1519518dffdada3e65aec98a6a838ba53fe
Shubham <shubham.gupta4@insight.com>
2025-11-28 22:25:08 +0530

    updated classes StringLengthWithoutUsingFunction.java and OnlyOneRepeatedString.java

--- src/practiceprograms/important/OnlyOneRepeatedString.java
+++ src/practiceprograms/important/OnlyOneRepeatedString.java
@@ -8,29 +8,72 @@ public class OnlyOneRepeatedString {
 
 	public static void main(String[] args) {
 		String str = "This $ is an $ automation $ interview";
+		int firstDollarIndex = str.indexOf('$');
+		System.out.println("Original String: "+str);
+//		METHOD 1 | BEGINNER | USING INDEXOF AND SUBSTRING COMBINATION | 
+//		TIME COMPLEXITY = o(n^2) | EASY TO UNDERSTAND | (TO BE TOLD IN INTERVIEW)
 		// Find the index of first dollar
-		int firstDollarIndex = str.indexOf('$'); 
+
+//		if (firstDollarIndex == -1) {
+//			System.out.println(
+//					"No dollars exist in the original string...So " + 
+//			"result string is the original string: " + str);
+//
+//		}
+//
+//		else {
+//			// Find the first sub-string up to first dollar
+//			String firstSub = str.substring(0, firstDollarIndex + 1);
+//
+//			// Find next sub-string after the first dollar
+//			String secondSub = str.substring(firstDollarIndex + 1, str.length());
+//
+//			// Splitting the second SubString from dollars
+//			String arr[] = secondSub.split("\\$");
+//
+//			// Iterating the array obtained after splitting
+//			// the second substring and appending each element
+//			// which is also a substring (part of second substring)
+//			// to the original string
+//			for (int i = 0; i < arr.length; i++) {
+//				arr[i] = arr[i].trim(); // trimming all leading and trailing
+//				// spaces in each element of the array
+//				str = firstSub + " " + arr[i];
+//				firstSub = str;
+//			}
+//			System.out.println("String: " + str);
+//		}
 		
-		// Find the first sub-string up to first dollar
-		String firstSub = str.substring(0,firstDollarIndex+1);
-		
-		// Find next sub-string after the first dollar
-		String secondSub = str.substring(firstDollarIndex+1,str.length());
-		
-		// Splitting the second SubString from dollars
-		String arr[] = secondSub.split("\\$");
-		
-		// Iterating the array obtained after splitting
-		// the second substring and appending each element
-		// which is also a substring (part of second substring)
-		// to the original string
-		for(int i = 0; i<arr.length; i++) {
-			arr[i] = arr[i].trim(); // trimming all leading and trailing
-			// spaces in each element of the array
-			str = firstSub+" "+arr[i];
-			firstSub = str;
+//		METHOD 2 | INTERMEDIATE | USING INDEXOF AND SUBSTRING COMBINATION | 
+//		TIME COMPLEXITY = o(n) | EASY TO UNDERSTAND | (TO BE TOLD IN INTERVIEW)
+		if (firstDollarIndex == -1) {
+			System.out.println(
+					"No dollars exist in the original string...So " + 
+			"result string is the original string: " + str);
+
 		}
-		System.out.println("String: " + str);
+		else {
+			// First sub-string up to first dollar occurrence
+			String firstSub = str.substring(0, firstDollarIndex+1);
+			
+			// Second sub-string after first dollar occurrence
+			String secSub = str.substring(firstDollarIndex+1);
+			
+			// Replacing all dollars in the 2nd sub-string 
+			// with only empty string
+			secSub = secSub.replace("$", "");
+			
+			// Replacing additional spaces created by removing
+			// dollars with empty string ("") with
+			// just single space in the 2nd sub-string
+			secSub = secSub.replaceAll(" +", " ");
+			
+			// Concatenating 1st and 2nd substrings to get
+			// the final string
+			str = firstSub + secSub;
+			
+			System.out.println("Final String: "+str);
+		}
 
 	}
 
---
+++ src/practiceprograms/important/StringLengthWithoutUsingFunction.java
@@ -0,0 +1,24 @@
+package src.practiceprograms.important;
+import java.text.*;
+
+public class StringLengthWithoutUsingFunction {
+	// Write a program to find the length of a string without using length().
+	public static void main(String[] args) {
+		String str = "This $ is an $ automation $ interview";
+//		// METHOD 1: USING SIMPLE LOOPS AND COUNT | (TO BE TOLD IN INTERVIEW)
+//		int count = 0;
+//		char[] charArr = str.toCharArray();
+//		for(int i = 0; i<charArr.length; i++) {
+//			count++;
+//		}
+//		System.out.println("String length: " +count);
+		
+		// METHOD 2: USING STRING CHARACTERITERATOR | (TO BE TOLD IN INTERVIEW)
+		CharacterIterator iterator = new StringCharacterIterator(str);
+		int lastIndex = iterator.getEndIndex();
+		
+		System.out.println("String length: "+lastIndex);
+
+	}
+
+}
