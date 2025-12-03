cba12b678e6d4c926d29c68624c79a36de3223ec
Shubham <shubham.gupta4@insight.com>
2025-12-03 22:49:18 +0530

    updated SwapNumbersWithoutTemp.java StringDiff.java SwapStringWithoutTemp.java

---
+++ src/practiceprograms/important/SwapNumbersWithoutTemp.java
@@ -0,0 +1,54 @@
+package src.practiceprograms.important;
+
+import java.util.Scanner;
+
+// Write a program to swap 2 numbers without using a temp variable
+
+public class SwapNumbersWithoutTemp {
+
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main(String args[]) {
+
+		double a, b;
+		System.out.println("Enter the 2 numbers: ");
+		System.out.print("a= ");
+		a = scan.nextDouble();
+		System.out.print("b= ");
+		b = scan.nextDouble();
+
+		// METHOD 1. Using arithmetic addition and subtraction
+//		a = a + b;
+//		b = a - b;
+//		a = a - b;
+//		System.out.println("Numbers after swap:");
+//		System.out.println("a= " + (int) a);
+//		System.out.println("b= " + (int) b);
+
+		// METHOD 2. Using XOR operator
+		// Returns 1 for X XOR Y if there are odd number of 1s i.e., 1,3,5.....so on
+		// number
+		// of ones else it will return 0.
+//        a = a ^ b;
+//        b = a ^ b;
+//        a = a ^ b;
+//        System.out.println("a= " + a);
+//        System.out.println("b= " + b);
+
+		// METHOD 3: USING ARITHMETIC DIVISION AND MULTIPLICATION
+		a = a * b;
+		b = a / b;
+		a = a / b;
+		System.out.println("Numbers after swap:");
+		System.out.print("a= " + (int) a);
+		System.out.println();
+		System.out.print("b= " + (int) b);
+
+		scan.close();
+		// NOTE: Please note that in Method 1 or Method 3 change the value of
+		// the variable in which you stored the SUM or PRODUCT of both
+		// the variables, at last. Otherwise, it will calculate
+		// incorrect/unexpected values.
+
+	}
+}
--- src/practiceprograms/important/SwapStringWithoutTemp.java
+++ src/practiceprograms/important/SwapStringWithoutTemp.java
@@ -1,45 +1,39 @@
-package src.practiceprograms;
+package src.practiceprograms.important;
 
 import java.util.Scanner;
 
 public class SwapStringWithoutTemp {
+	// WAP In JAVA to Swap 2 Strings WITHOUT using
+	// TEMP/THIRD variable
 
 	public static void main(String[] args) {
 
 		// Method 1. Using "+" concatenation operator
 
-		String str1, str2; // str1  = java
-		                   // str2 = world
+		String str1, str2; // str1 = java
+							// str2 = world
 
 		System.out.println("Enter String1:");
-
 		Scanner scan = new Scanner(System.in);
-
 		str1 = scan.nextLine();
-
 		System.out.println("Enter String2:");
-
 		str2 = scan.nextLine();
-
 		scan.close();
 
 		System.out.println("-------Before Swapping------");
-
 		System.out.println("str1:" + str1);
-
 		System.out.println("str2:" + str2);
-
 		str1 = str1 + str2; // str1 = javaworld
-
 		str2 = str1.substring(0, str1.length() - str2.length()); // str2 = java
-
 		str1 = str1.substring(str2.length()); // str1 = world
-
 		System.out.println("-------After Swapping------");
-
 		System.out.println("str1:" + str1);
-
 		System.out.println("str2:" + str2);
+
+		// NOTE: Please note that in this Method change the value of
+		// the string in which you stored the SUM/CONCATENATION of both
+		// the strings, at last. Otherwise, it will calculate
+		// incorrect/unexpected values.
 
 	}
 
