22a4e34f9f158ed12a18f0c24f4842b88a25e0b5
Shubham <shubham.gupta4@insight.com>
2025-12-12 22:48:21 +0530

    Added class file LargestPossibleNoFromAnArrayOfInt.java

--- src/practiceprograms/importantinterviewquestions/LargestPossibleNoFromAnArrayOfInt.java
+++ src/practiceprograms/importantinterviewquestions/LargestPossibleNoFromAnArrayOfInt.java
@@ -16,6 +16,33 @@ public class LargestPossibleNoFromAnArrayOfInt {
 
 	public static void main(String[] args) {
 		
+		// METHOD 1 - BEGINNER| USING SIMPLE LOOPS, BUBBLE SORT AND COMPARETO
+		// METHOD OF STRING CLASS TO LEXICOGRAPHICALLY ORDERING THE STRINGS| EASY TO UNDERSTAND
+		// | TO BE TOLD IN INTERVIEW
+		
+		int[] arr = {3,30,32,7,319};
+		
+		// BUBBLE SORTING THE ELEMENTS
+		// BY CONVERTING EACH OF THE ELEMENT TO STRING
+		// AND LEXICOGRAPHICALLY ORDERING EACH OF THEM USING
+		// COMPARETO METHOD
+		for(int i =0; i<arr.length; i++) {
+			for(int j = 0; j<arr.length-1; j++) {
+				String concat1 = String.valueOf(arr[j]) + String.valueOf(arr[j+1]);
+				String concat2 = String.valueOf(arr[j+1]) + String.valueOf(arr[j]);
+				if(concat2.compareTo(concat1) > 0) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		
+		String result = "";
+		for(int num : arr) {
+			result+=num;
+		}
+		System.out.println("Largest possible number from the array: "+ result);
 
 	}
 
